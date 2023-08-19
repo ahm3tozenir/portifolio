85450a367a6a3af77ea8eabe79463d615d30585b
ahm3tozenir <ozenirahm3t@gmail.com>
2023-08-19 15:59:16 +0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "https://ahm3tozenir.github.io/portifolio/"
+  plugins: [react()]
 })
 
